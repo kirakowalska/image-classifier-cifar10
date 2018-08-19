12306e1d5b00774c5aed5b085b7710ca97fcc3b2
Kira Kempinska <kira.kowalska.13@ucl.ac.uk>
2018-08-19 18:09:08 +0100

    Use keras for data import

--- implot.py
+++ implot.py
@@ -1,20 +1,23 @@
-from load_cifar import load_batch
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-images , labels = load_batch()
-images = np.rollaxis(np.reshape(images, (10000, 3, 32, 32)), 1, 4)
+# Import data
+from keras.datasets import cifar10
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+y_train = y_train.flatten()
+y_test  = y_test.flatten()
 
 n=0
 fig = plt.figure()
 for c in range(0,10):
     # Find images in class c
-    idxs = np.where(labels == c)[0][0:10]
+    idxs = np.where(y_train == c)[0][0:10]
 
     # Add them to the plot
     for i in idxs:
         a = fig.add_subplot(10, 10, n + 1)
-        plt.imshow(images[i,:,:,:])
+        plt.imshow(X_train[i,:,:,:])
         plt.axis("off")
         n+=1
 
